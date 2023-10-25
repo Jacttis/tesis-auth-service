0696d5bceb479029996d08229baf8d2edc33a435
jacttis <julianacttis@hotmail.com>
2023-10-24 23:59:34 -0300

    uncomment important code

--- src/main/java/com/example/Auth/service/WorkerManager.java
+++ src/main/java/com/example/Auth/service/WorkerManager.java
@@ -34,8 +34,7 @@ public class WorkerManager implements UserDetailsManager {
 
         WorkerMessage workerMessage = new WorkerMessage();
         workerMessage.setWorkerDTO(WorkerDTO.from((Worker) worker));
-
-       // workerMessagePublisher.publishMessage(workerMessage);
+        workerMessagePublisher.publishMessage(workerMessage);
 
 
     }
