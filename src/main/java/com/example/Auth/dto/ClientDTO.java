19a261c01dfa8a345b5250bf13376a93470c0d3c
LeanM <42581931+LeanM@users.noreply.github.com>
2023-11-01 13:49:47 -0300

    Fixed ClientDTO builder with no all attributes

--- src/main/java/com/example/Auth/dto/ClientDTO.java
+++ src/main/java/com/example/Auth/dto/ClientDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
+import java.util.Date;
+
 @Builder
 @Data
 public class ClientDTO {
@@ -12,10 +14,12 @@ public class ClientDTO {
     private String address;
     private Double latitude;
     private Double longitude;
+    private String picture;
     private String name;
     private String phoneNumber;
+    private Date birthDate;
 
     public static ClientDTO from(Client client) {
-        return builder().email(client.getEmail()).address(client.getAddress()).latitude(client.getLatitude()).longitude(client.getLongitude()).name(client.getName()).build();
+        return builder().email(client.getEmail()).address(client.getAddress()).latitude(client.getLatitude()).longitude(client.getLongitude()).name(client.getName()).phoneNumber(client.getPhoneNumber()).birthDate(client.getBirthDate()).picture(client.getPicture()).build();
     }
 }
